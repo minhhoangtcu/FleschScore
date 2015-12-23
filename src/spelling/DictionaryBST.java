bf9a8a44f803db540c2bba91b7d1be6de8828a17
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-12-23 01:05:10 -0600

    Finished the dictionary that uses a bst (balanced)

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -10,9 +10,9 @@ public class DictionaryBST implements Dictionary
 {
    private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
+   public DictionaryBST() {
+	   dict = new TreeSet<>();
+   }
     
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
@@ -20,22 +20,29 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    	if (isWord(word))
+    		return false;
+    	else {
+    		word = word.toLowerCase();
+    		dict.add(word);
+    		return true;
+    	}
     }
 
 
     /** Return the number of words in the dictionary */
-    public int size()
-    {
-    	// TODO: Implement this method
-        return 0;
+    public int size() {
+    	return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
-    public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+    public boolean isWord(String word) {
+    	if (size() == 0)
+    		return false;
+    	else {
+    		word = word.toLowerCase();
+        	return dict.contains(word);
+    	}
     }
 
 }
