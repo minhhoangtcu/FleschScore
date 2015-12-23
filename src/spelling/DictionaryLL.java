2a898c1cbfd77e2c8070fd5e6d7e21b2666f1fad
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-12-23 01:01:16 -0600

    Finished the dictionary that uses a LinkedList.

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -6,37 +6,49 @@ import java.util.LinkedList;
  * A class that implements the Dictionary interface using a LinkedList
  *
  */
-public class DictionaryLL implements Dictionary 
-{
+public class DictionaryLL implements Dictionary {
 
 	private LinkedList<String> dict;
-	
-    // TODO: Add a constructor
 
+	public DictionaryLL() {
+		dict = new LinkedList<>();
+	}
 
-    /** Add this word to the dictionary.  Convert it to lowercase first
-     * for the assignment requirements.
-     * @param word The word to add
-     * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
-    public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
-    }
+	/**
+	 * Add this word to the dictionary. Convert it to lowercase first for the
+	 * assignment requirements.
+	 * 
+	 * @param word
+	 *            The word to add
+	 * @return true if the word was added to the dictionary (it wasn't already
+	 *         there).
+	 */
+	public boolean addWord(String word) {
+		word = word.toLowerCase();
+		if (!isWord(word)) {
+			dict.add(word);
+			return true;
+		} else
+			return false;
+	}
 
+	/** Return the number of words in the dictionary */
+	public int size() {
+		return dict.size();
+	}
 
-    /** Return the number of words in the dictionary */
-    public int size()
-    {
-        // TODO: Implement this method
-        return 0;
-    }
+	/** Is this a word according to this dictionary? */
+	public boolean isWord(String word) {
+		word = word.toLowerCase();
+		if (size() == 0) // if the list is empty, then the provided word cannot be inside the list
+			return false;
+		else {
+			for (String string : dict) {
+				if (word.equals(string))
+					return true;
+			}
+			return false;
+		}
+	}
 
-    /** Is this a word according to this dictionary? */
-    public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
-    }
-
-    
 }
